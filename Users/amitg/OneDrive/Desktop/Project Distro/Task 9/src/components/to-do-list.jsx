a30729e4a5632e9d543fabebec4efb452f5f344f
Amit Gahatraj <amitgahatraj4@gmail.com>
2024-08-09 23:06:51 -0400

    Created components for ToDo list

---
+++ Users/amitg/OneDrive/Desktop/Project Distro/Task 9/src/components/to-do-list.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./form";
+
+const TodoList = () => {
+  return (
+    <div className="container bg-blue-950 mt-20 p-8 rounded-md">
+      <Form />
+    </div>
+  );
+};
+
+export default TodoList;
